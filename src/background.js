8c9fb55b1a431d261a513d095eae95d287fece29
jungyw <jungyw@emforce.co.kr>
2020-07-31 13:21:39 +0900

    complete

--- src/background.js
+++ src/background.js
@@ -22,22 +22,26 @@ chrome.storage.local.clear();
 
 const tabs = {};
 
+// 탭 선택시
 chrome.tabs.onActivated.addListener(activeInfo => {
   const trackList = tabs[activeInfo.tabId] || [];
   setBadgeText(trackList.length);
   setListToStorage(trackList);
 });
 
+// 신규 탭 생성시 초기화
 chrome.tabs.onCreated.addListener(tab => {
   initTabById(tab.id);
 });
 
+// 탭이 새로 로딩되거나 페이지 변경됐거나 했을 때
 chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
   if (changeInfo.status === 'loading') {
     initTabById(tabId);
   }
 });
 
+// API 호출시 후킹
 chrome.webRequest.onBeforeRequest.addListener(
   ({ tabId, url, requestBody, method }) => {
     if (!['GET', 'POST'].includes(method)) {
@@ -59,6 +63,7 @@ chrome.webRequest.onBeforeRequest.addListener(
   ['requestBody']
 );
 
+// analytics.js 로드 완료시. 나중에 처리할 일 있으면 처리.
 chrome.webRequest.onCompleted.addListener(
   detail => {
     console.log(detail);
